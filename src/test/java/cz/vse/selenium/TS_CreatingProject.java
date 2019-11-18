3ecd72dd8aa756772c6ee23903cc390433979092
Collda <48909756+Collda@users.noreply.github.com>
2019-11-18 16:27:29 +0100

    Update TS_CreatingProject.java
    delettin non used imports

--- src/test/java/cz/vse/selenium/TS_CreatingProject.java
+++ src/test/java/cz/vse/selenium/TS_CreatingProject.java
@@ -5,22 +5,15 @@ import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
 import org.openqa.selenium.By;
-import org.openqa.selenium.Keys;
-import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
-import org.openqa.selenium.chrome.ChromeDriverService;
-import org.openqa.selenium.chrome.ChromeOptions;
-import org.openqa.selenium.support.ui.ExpectedCondition;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.Select;
 import org.openqa.selenium.support.ui.WebDriverWait;
-
 import java.util.List;
 import java.util.Date;
 import java.text.SimpleDateFormat;
 import java.text.ParseException;
-import java.util.UUID;
 
 public class TS_CreatingProject {
 
